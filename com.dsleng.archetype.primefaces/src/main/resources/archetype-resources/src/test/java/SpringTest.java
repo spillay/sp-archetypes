9a64391ac8003b4f496ebf607dd3a398f878e683
suresh <suresh@workspaceafrica.com>
2015-10-16 19:50:30 +0200

    modified pf

---
+++ com.dsleng.archetype.primefaces/src/main/resources/archetype-resources/src/test/java/SpringTest.java
@@ -0,0 +1,37 @@
+package ${package};
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-servlet.xml"})
+@Transactional
+public class SpringTest {
+
+	//@Autowired
+	//ClientService dao;
+	
+	@Test
+	public void Test_PermitControlImpl() {
+		 try {
+				//List<Client> clients = dao.getAll();
+				//if ( clients.size() > 0){
+				//	PermitInfo o = new PermitInfo(clients.get(0));
+				//	System.out.print(o.getContent());
+				//}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+}
